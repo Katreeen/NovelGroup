c1a528d2829492b63169e0b5a97296aa6e12a16b
Katreen <zolotaevaekaterina@gmail.com>
2021-04-14 17:39:35 +0400

    start animation

--- js/common.js
+++ js/common.js
@@ -35,4 +35,67 @@ $(document).ready(function () {
 		focusOnSelect: true
 	 });
 	
+	new WOW().init();
+
+
+	SmoothScroll({
+		// Время скролла 400 = 0.4 секунды
+		animationTime    : 800,
+		// Размер шага в пикселях 
+		stepSize         : 75,
+  
+		// Дополнительные настройки:
+		
+		// Ускорение 
+		accelerationDelta : 30,  
+		// Максимальное ускорение
+		accelerationMax   : 2,   
+  
+		// Поддержка клавиатуры
+		keyboardSupport   : true,  
+		// Шаг скролла стрелками на клавиатуре в пикселях
+		arrowScroll       : 50,
+  
+		// Pulse (less tweakable)
+		// ratio of "tail" to "acceleration"
+		pulseAlgorithm   : true,
+		pulseScale       : 4,
+		pulseNormalize   : 1,
+  
+		// Поддержка тачпада
+		touchpadSupport   : true,
+	});
+	
+
+	const main = document.querySelector('.main');
+	const tl = gsap.timeline();
+	tl.fromTo(".main_carousel .carousel_item_title", { y: 0 }, { y: '-100%' });
+	//tl.fromTo(".main_carousel .carousel_item", { y: 0 }, { y: '-100%' });
+	//end: () => main.offsetWidth / 2;
+
+	ScrollTrigger.create({
+		animation: tl,
+		trigger: '.main_carousel',
+		start: 'top top',
+		end: 'bottom',
+		scrub: true,
+		//pin: true
+	});
+
+	const tlAbout = gsap.timeline();
+	tlAbout.fromTo(".about_img", { y: 0 }, { y: '-100%', duration: 1})
+	tlAbout.fromTo(".about_text", { y: 0 }, { y: '-100%', duration: 1})
+	tlAbout.fromTo(".about_bottom", { y: 0 }, { y: '-100%', duration: 1 });
+
+	ScrollTrigger.create({
+		animation: tlAbout,
+		trigger: '.section_about',
+		start: 'top top',
+		end: 'bottom',
+		scrub: true,
+		//pin: true
+	});
+	
+
+
 });
