ee85ec313b7708808a6fd840a6e142a9f093fe3c
Katreen <zolotaevaekaterina@gmail.com>
2021-04-27 13:02:18 +0400

    new-pages

--- js/common.js
+++ js/common.js
@@ -75,6 +75,16 @@ $(document).ready(function () {
 		 ]
 		
 	 });
+	 $('.recomended_carousel').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 1000,
+		slidesToShow: 4,
+		variableWidth: true
+		//centerMode: true,
+		//adaptiveHeight: true
+	});
 	
 	
 	
@@ -124,16 +134,19 @@ $(document).ready(function () {
 	
 
 	window.onresize = function (event) {
+		if ($(".pl-js").length){
+			var viewportOffset = $('.offset-xxl-2')[0].getBoundingClientRect();
+			var left = viewportOffset.left;
+			//console.log(left);
+			$('.pl-js').css( "padding-left", left);
+		}
+	};
+	if ($(".pl-js").length){
 		var viewportOffset = $('.offset-xxl-2')[0].getBoundingClientRect();
 		var left = viewportOffset.left;
 		//console.log(left);
 		$('.pl-js').css( "padding-left", left);
-	};
-	var viewportOffset = $('.offset-xxl-2')[0].getBoundingClientRect();
-	var left = viewportOffset.left;
-	//console.log(left);
-	$('.pl-js').css( "padding-left", left);
-
+	}
 	
 	$('.navbar-toggler').click(function () {
 		$('.header_drop').slideDown();
@@ -143,6 +156,68 @@ $(document).ready(function () {
 	})
 
 
+	var $tabs = function (target) {
+		var
+			_elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
+			_eventTabsShow,
+			_showTab = function (tabsLinkTarget) {
+				var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
+				tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
+				tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
+				tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
+				// если следующая вкладка равна активной, то завершаем работу
+				if (tabsLinkTarget === tabsLinkActive) {
+					return;
+				}
+				// удаляем классы у текущих активных элементов
+				if (tabsLinkActive !== null) {
+					tabsLinkActive.classList.remove('tabs__link_active');
+				}
+				if (tabsPaneShow !== null) {
+					tabsPaneShow.classList.remove('tabs__pane_show');
+				}
+				// добавляем классы к элементам (в завимости от выбранной вкладки)
+				tabsLinkTarget.classList.add('tabs__link_active');
+				tabsPaneTarget.classList.add('tabs__pane_show');
+				document.dispatchEvent(_eventTabsShow);
+			},
+			_switchTabTo = function (tabsLinkIndex) {
+				var tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
+				if (tabsLinks.length > 0) {
+					if (tabsLinkIndex > tabsLinks.length) {
+						tabsLinkIndex = tabsLinks.length;
+					} else if (tabsLinkIndex < 1) {
+						tabsLinkIndex = 1;
+					}
+					_showTab(tabsLinks[tabsLinkIndex - 1]);
+				}
+			};
+		
+		_eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
+		
+		_elemTabs.addEventListener('click', function (e) {
+			var tabsLinkTarget = e.target;
+			// завершаем выполнение функции, если кликнули не по ссылке
+			if (!tabsLinkTarget.classList.contains('tabs__link')) {
+				return;
+			}
+			// отменяем стандартное действие
+			e.preventDefault();
+			_showTab(tabsLinkTarget);
+		});
+		
+		return {
+			showTab: function (target) {
+				_showTab(target);
+			},
+			switchTabTo: function (index) {
+				_switchTabTo(index);
+			}
+		}
+		
+		};
+		
+		$tabs('.tabs');
 
 
 
